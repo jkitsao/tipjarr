984bd78ee34aa7302fb696eafb1796788dab3434
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-10-30 23:33:26 +0300

    remove copyright element

--- components/landing_page/components/tips/Jumbo.js
+++ components/landing_page/components/tips/Jumbo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Jumbo() {
   return (
-    <div class="lg:h-64 bg-gradient-to-r from-teal-400 to-blue-500 rounded-lg">
+    <div class="lg:h-64 bg-gradient-to-r from-teal-400 to-blue-500 rounded-lg mx-2">
       <div className="p-6 sm:p-10">
         <h2 className="text-lg lg:text-2xl text-center font-semibold text-white ">
           Tipjarr Allows you to share and discover web development tips and
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home({ data }) {
     <div>
       {/* <h2 className="text-2xl text-teal-500 text-center">hello there people</h2> */}
       <Main_page data={data} />
-      <span className="fixed bottom-0 opacity-75 right-0 mx-2 my-2 p-2 rounded text-xs sm:text-sm bg-gray-700 text-gray-300 ">
+      {/* <span className="fixed bottom-0 opacity-75 right-0 mx-2 my-2 p-2 rounded text-xs sm:text-sm bg-gray-700 text-gray-300 ">
         Built and maintained by{" "}
         <a
           href="https://twitter.com/Jacksonkitsao5"
@@ -18,7 +18,7 @@ export default function Home({ data }) {
         <a href="https://github.com/jkitsao" className="text-blue-400">
           Github
         </a>
-      </span>
+      </span> */}
     </div>
   );
 }
